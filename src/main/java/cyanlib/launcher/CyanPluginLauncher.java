e126baeb53973893ff25ec86eaf309d5e8d2e1eb
SmallXY <2569224983@qq.com>
2024-03-19 18:44:57 +0800

    更新 src/main/java/cyanlib/launcher/CyanPluginLauncher.java

--- src/main/java/cyanlib/launcher/CyanPluginLauncher.java
+++ src/main/java/cyanlib/launcher/CyanPluginLauncher.java
@@ -17,7 +17,17 @@ public class CyanPluginLauncher extends JavaPlugin {
     }
 
 
-
+    public void registerCommand(Command command) {
+        Class<?> pluginManagerClass = cyanPlugin.getServer().getPluginManager().getClass();
+        try {
+            Field field = pluginManagerClass.getDeclaredField("commandMap");
+            field.setAccessible(true);
+            SimpleCommandMap commandMap = (SimpleCommandMap) field.get(cyanPlugin.getServer().getPluginManager());
+            commandMap.register(cyanPlugin.getName(), command);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void onLoad() {
