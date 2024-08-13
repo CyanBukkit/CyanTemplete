d3ac673e05e67d65fe40c1122ad5752579b85bf9
SmallXY <2569224983@qq.com>
2024-08-13 15:41:55 +0800

    更新 src/main/java/cn/cyanbukkit/example/cyanlib/launcher/JavaHandle.java

--- src/main/java/cn/cyanbukkit/example/cyanlib/launcher/JavaHandle.java
+++ src/main/java/cn/cyanbukkit/example/cyanlib/launcher/JavaHandle.java
@@ -6,15 +6,13 @@ public class JavaHandle {
 
     public static void getJavaVersionToDownloadHikaricp() {
         String javaVersion = System.getProperty("java.version");
-        String[] split = javaVersion.split("\\.");
-        int version = Integer.parseInt(split[1]);
-        if (version == 8) {
+        if (javaVersion.contains("8")) {
             KotlinBootstrap.loadDepend("com.zaxxer","HikariCP","4.0.3");
-        } else if (version >= 11) {
+        } else if (javaVersion.contains("11")) {
             KotlinBootstrap.loadDepend("com.zaxxer","HikariCP","5.1.0");
-        } else if (version == 7) {
+        } else if (javaVersion.contains("7")) {
             KotlinBootstrap.loadDepend("com.zaxxer","HikariCP","2.4.13");
-        } else if (version == 6) {
+        } else if (javaVersion.contains("6")) {
             KotlinBootstrap.loadDepend("com.zaxxer","HikariCP","2.3.13");
         } else {
             System.out.println("Java 8");
