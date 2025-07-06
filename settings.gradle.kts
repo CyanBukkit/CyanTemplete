037c1acfce3b80f469c789b0990aae18430507f8
SmallXY <62281180+SmallXY@users.noreply.github.com>
2025-07-06 19:08:15 +0800

    remove coding

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,5 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
-//        操Fabric的API版本
-        maven("https://lanternmc-maven.pkg.coding.net/repository/cyanbukkit/fabric/") {
-            name = "Fabric"
-        }
     }
 }
